5d42ef22879b4cfdf527f231515063a728274da0
Eric Noll <eanoll88@gmail.com>
2019-10-21 21:14:39 -0500

    styling update

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ $(document).ready(function() {
     $("#date").html(`Today is: ${todaysDate}, ${todaysTime}`);
     var currentHour = moment().hours();
     console.log(currentHour);
-    var inputTime = $("input");
+    var inputTime = $("button");
   }
   // helper functions
-
   // events
   // clicking save button
   $(".js-save").on("click", function() {
@@ -26,6 +25,15 @@ $(document).ready(function() {
   $("hour-9").val(tasks["hour-9"]);
   $("hour-10").val(tasks["hour-10"]);
   $("hour-11").val(tasks["hour-11"]);
+  $("hour-12").val(tasks["hour-12"]);
+  $("hour-1").val(tasks["hour-1"]);
+  $("hour-2").val(tasks["hour-2"]);
+  $("hour-3").val(tasks["hour-3"]);
+  $("hour-4").val(tasks["hour-4"]);
+  $("hour-5").val(tasks["hour-5"]);
+  $("hour-6").val(tasks["hour-6"]);
+  $("hour-7").val(tasks["hour-7"]);
+  $("hour-8").val(tasks["hour-8"]);
   // check local storage object for matching id
   // set todays date in the header
   // color code past, current, and future bars
